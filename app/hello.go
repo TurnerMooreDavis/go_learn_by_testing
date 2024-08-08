cfd20786165c80b7f3e0ffa5596ea62fcba68538
TurnerMooreDavis <turnermdavis@gmail.com>
2024-08-07 20:41:17 -0400

    testing and adding second argument

--- app/hello.go
+++ app/hello.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+const suffix = " SAY WHAT!?!"
+const reverseResponse = "YOU GOT ME "
+const reverse = "no u"
+
 func main() {
-	fmt.Println(SayWhat("hello"))
+	fmt.Println(SayWhat("hello", ""))
 }
 
-func SayWhat(s string) string {
-	return s
+func SayWhat(s string, s2 string) string {
+	if s2 == reverse {
+		return reverseResponse + s + "!"
+	} else if s == "" {
+		return "Dummy" + suffix
+	}
+	return s + suffix
 }
--- app/hello_test.go
+++ app/hello_test.go
@@ -3,8 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	var got string = SayWhat("Turner")
-	want := "Turner"
+	t.Run("name say what", func(t *testing.T) {
+		var got string = SayWhat("Turner", "")
+		want := "Turner" + suffix
+		assert(t, got, want)
+	})
+	t.Run("if no name given say 'Dummy SAY WHAT!?!'", func(t *testing.T) {
+		var got string = SayWhat("", "")
+		want := "Dummy" + suffix
+		assert(t, got, want)
+	})
+
+	t.Run("if given a second argument 'no u' and first argument 'Turner' say 'YOU GOT ME Turner!'", func(t *testing.T) {
+		var got string = SayWhat("Turner", "no u")
+		want := "YOU GOT ME Turner!"
+		assert(t, got, want)
+	})
+}
+
+func assert(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
